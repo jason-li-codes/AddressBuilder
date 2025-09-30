0184b811014c7cf9acb993bde86374fce9d23dca
jasonli <lijason991@gmail.com>
2025-09-30 11:51:18 -0400

    second commit

--- src/main/java/com/pluralsight/AddressBuilder.java
+++ src/main/java/com/pluralsight/AddressBuilder.java
@@ -9,35 +9,55 @@ public class AddressBuilder {
 
         Scanner input = new Scanner(System.in);
 
+        StringBuilder billingAddress = new StringBuilder();
+
         System.out.println("Please provide the following information: ");
 
         System.out.println("Full name: ");
-        String name = input.nextLine();
-        System.out.println("Billing Street: ");
-        String billingStreet = input.nextLine();
-        System.out.println("Billing City: ");
-        String billingCity = input.nextLine();
-        System.out.println("Billing State: ");
-        String billingState = input.nextLine();
-        System.out.println("Billing ZIP: ");
-        String billingZIP = input.nextLine();
+        billingAddress.append(input.nextLine());
+        billingAddress.append("\n \nBuilding Address: \n");
 
-        StringBuilder billingAddress = new StringBuilder();
+        System.out.println("Billing Street: ");
+        billingAddress.append(input.nextLine());
+        billingAddress.append("\n");
+
+        System.out.println("Billing City: ");
+        billingAddress.append(input.nextLine());
+        billingAddress.append(", ");
+
+        System.out.println("Billing State: ");
+        billingAddress.append(input.nextLine());
+        billingAddress.append(" ");
+
+        System.out.println("Billing ZIP: ");
+        billingAddress.append(input.nextLine());
+
+        System.out.println(billingAddress.toString());
+
+        StringBuilder shippingAddress = new StringBuilder();
 
         System.out.println("Is your shipping address different from your billing address? Y/N");
         char diffAddresses = Character.toLowerCase(input.nextLine().charAt(0));
 
         if (diffAddresses == 'y') {
+
             System.out.println("Shipping Street: ");
-            String shippingStreet = input.nextLine();
+            shippingAddress.append("Shipping Address: \n");
+            shippingAddress.append(input.nextLine());
+            shippingAddress.append("\n");
+
             System.out.println("Shipping City: ");
-            String shippingCity = input.nextLine();
+            shippingAddress.append(input.nextLine());
+
             System.out.println("Shipping State: ");
             String shippingState = input.nextLine();
+
             System.out.println("Shipping ZIP: ");
             String shippingZIP = input.nextLine();
         }
-
+        else {
+            shippingAddress.append(billingAddress);
+        }
 
 
 

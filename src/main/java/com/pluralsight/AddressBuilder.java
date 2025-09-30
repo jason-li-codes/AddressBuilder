ea8e43fe67a01a6058de647dc4e04e4064d96542
jasonli <lijason991@gmail.com>
2025-09-30 14:11:00 -0400

    added comments

--- src/main/java/com/pluralsight/AddressBuilder.java
+++ src/main/java/com/pluralsight/AddressBuilder.java
@@ -6,19 +6,24 @@ public class AddressBuilder {
 
     public static void main(String[] args) {
 
+        // names new scanner
         Scanner input = new Scanner(System.in);
 
         System.out.println("Please provide the following information: ");
 
+        // prompts user for full name first
         System.out.println("Full name: ");
         String name = input.nextLine();
 
+        // starts with billing address, then calls addressFormatter method to format
         System.out.println("Let's start with your billing address.");
         String billingAddress = addressFormatter(input);
 
+        // asks if shipping address is the same as billing address
         System.out.println("Let's enter your shipping address. Is it the same as your billing address? Y/N");
         char addressesSame = Character.toLowerCase(input.nextLine().charAt(0));
 
+        // based on that answer, either copies billing address or runs addressFormatter again
         String shippingAddress = "";
         if (addressesSame == 'y') {
             shippingAddress = billingAddress;
@@ -28,6 +33,7 @@ public class AddressBuilder {
             shippingAddress = addressFormatter(input);
         }
 
+        // prints final result with name, billing address, and shipping address
         System.out.printf("""
                 Here is your information:
                 %s
@@ -43,8 +49,10 @@ public class AddressBuilder {
 
     public static String addressFormatter(Scanner input) {
 
+        // starts a string builder
         StringBuilder buildingAddress = new StringBuilder();
 
+        // after each prompt, adds the user input to string builder with appropriate spacing and formatting
         System.out.println("Street: ");
         buildingAddress.append(input.nextLine());
         buildingAddress.append("\n");
@@ -60,6 +68,7 @@ public class AddressBuilder {
         System.out.println("ZIP: ");
         buildingAddress.append(input.nextLine());
 
+        // returns the formatted address by converting the string builder to a string
         return buildingAddress.toString();
     }
 
